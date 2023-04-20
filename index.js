5a3269e466590d592b27270ff173e1e640e06931
ravi-zologics <ravikant8921@gmail.com>
2023-04-20 03:19:17 -0700

    task8

--- index.js
+++
@@ -1,5 +0,0 @@
-var items =document.getElementsByClassName('list-grout-item')
-console.log(items);
-console.log(items[1]);
-items[1].style.fontweight  ='Bold';
-items.style.backgroundColor ='yellow'
